1424ba3aef9606a810a48330a92ff89581e23060
cotlowzky <cotlowzky@gmail.com>
2022-08-07 13:27:33 +0200

    add some stuff to home page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -5,6 +5,13 @@ export default function HomePage() {
   return (
       <div className='background w-100 h-100'>
         <TopNavbar />
+        <h1>Zapraszamy</h1>
+        <h2>Jesteśmy otwarci codziennie od 10 do 20</h2>
+        <div className='flex-column'>
+          <button>kup/zarezerwuj bilet</button>
+          <button>sprawdz zasady bezpieczeństwa</button>
+          <button>od poniedziałku do piatku w godz. 8:00 - 15:30 zapraszamy grupy zorganizowane (min. 10 osób)</button>
+        </div>
       </div>
   )
 }
