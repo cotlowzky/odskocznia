78e72b5ad64af697a1709ee172de6770a33180ca
cotlowzky <cotlowzky@gmail.com>
2022-08-07 13:10:06 +0200

    background color & top navbar

---
+++ src/Components/TopNavbar/TopNavbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function TopNavbar() {
+  return (
+    <header className='flex-row flex-justify-around'>
+        <p>logo</p>
+        <a href="#">zasady bezpieczeństwa</a>
+        <a href="#">regulamin</a>
+        <a href="#">faq</a>
+        <a href="#">regulamin urodzin</a>
+        <a href="#">HopMania</a>
+        <a href="#">Atrakcje</a>
+        <a href="#">Kontakt</a>
+    </header>
+  )
+}
---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TopNavbar from '../../Components/TopNavbar/TopNavbar'
+
+export default function HomePage() {
+  return (
+      <div className='background w-100 h-100'>
+        <TopNavbar />
+      </div>
+  )
+}
