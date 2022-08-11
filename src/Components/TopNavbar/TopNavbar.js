c49e8cf1349e4ff17118f0347b3edab6774f271b
cotlowzky <cotlowzky@gmail.com>
2022-08-12 00:00:02 +0200

    styling topnavbar

--- src/Components/TopNavbar/TopNavbar.js
+++ src/Components/TopNavbar/TopNavbar.js
@@ -2,15 +2,15 @@ import React from 'react'
 import './TopNavbar.css'
 export default function TopNavbar() {
   return (
-    <header className='flex-row flex-justify-around h-5 background-navbar flex-items-center'>
+    <header className='flex-row flex-justify-around h-5 background-navbar flex-items-center w-70 shadow p-2'>
         <p>logo</p>
-        <a href="#">zasady bezpieczeństwa</a>
-        <a href="#">regulamin</a>
-        <a href="#">faq</a>
-        <a href="#">regulamin urodzin</a>
-        <a href="#">HopMania</a>
-        <a href="#">Atrakcje</a>
-        <a href="#">Kontakt</a>
+        <a href="#" className='font-15 text-upper font-montserrat-medium'>zasady bezpieczeństwa</a>
+        <a href="#" className='font-15 text-upper font-montserrat-medium'>regulamin</a>
+        <a href="#" className='font-15 text-upper font-montserrat-medium'>faq</a>
+        <a href="#" className='font-15 text-upper font-montserrat-medium'>regulamin urodzin</a>
+        <a href="#" className='font-15 text-upper font-montserrat-medium'>HopMania</a>
+        <a href="#" className='font-15 text-upper font-montserrat-medium'>Atrakcje</a>
+        <a href="#" className='font-15 text-upper font-montserrat-medium'>Kontakt</a>
     </header>
   )
 }
