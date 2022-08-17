176a01bc69dc7002358034a3f1e6cc550bc6b5ef
cotlowzky <cotlowzky@gmail.com>
2022-08-17 20:31:52 +0200

    add react router and first link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import HomePage from './Pages/HomePage/HomePage';
 
+import { 
+  BrowserRouter,
+  Routes,
+  Route } from "react-router-dom";
+import FaqPage from './Pages/FaqPage/FaqPage';
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/faq" element={<FaqPage />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- src/Components/TopNavbar/TopNavbar.js
+++ src/Components/TopNavbar/TopNavbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './TopNavbar.css'
+import { Link } from "react-router-dom"
 export default function TopNavbar() {
   return (
     <header className='flex-row flex-justify-around h-5 background-navbar flex-items-center w-70 shadow p-2'>
         <p>logo</p>
         <a href="#" className='font-15 text-upper font-montserrat-medium'>zasady bezpieczeństwa</a>
         <a href="#" className='font-15 text-upper font-montserrat-medium'>regulamin</a>
-        <a href="#" className='font-15 text-upper font-montserrat-medium'>faq</a>
+        <Link to="/faq" className='font-15 text-upper font-montserrat-medium'>faq</Link>
         <a href="#" className='font-15 text-upper font-montserrat-medium'>regulamin urodzin</a>
         <a href="#" className='font-15 text-upper font-montserrat-medium'>HopMania</a>
         <a href="#" className='font-15 text-upper font-montserrat-medium'>Atrakcje</a>
