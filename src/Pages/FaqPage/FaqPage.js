ad11bc7e7a60a6cc00650e0b044d5976279bf3ab
cotlowzky <cotlowzky@gmail.com>
2022-08-18 23:55:27 +0200

    add content on faq

--- src/Pages/FaqPage/FaqPage.js
+++ src/Pages/FaqPage/FaqPage.js
@@ -8,7 +8,8 @@ function FaqPage() {
       q1:false,
       q2:false,
       q3:false,
-      q4:false
+      q4:false,
+      q5:false
     }
     const [shownQuestions, setShownQuestions] = useState(allQuestions);
     const showQuestion = (event,key)=>{
@@ -38,11 +39,13 @@ function FaqPage() {
             <FaqComponentQ onClick={(e)=>showQuestion(e,'q1')}question="Co trzeba wiedzieć przed pierwszą wizytą w parku trampolin?"/>
             <FaqComponentA answer="kaka1" />
             <FaqComponentQ onClick={(e)=>showQuestion(e,'q2')} question="Od jakiego wieku dzieci mogą korzystać z parku trampolin?"/>
-            <FaqComponentA answer="kaka2" />
+            <FaqComponentA answer="Na co dzień z atrakcji mogą korzystać osoby dorosłe i dzieci od 5 roku życia. Dzieci w wieku 5-6 lat mogą bawić się tylko pod opieką osoby dorosłej." />
             <FaqComponentQ onClick={(e)=>showQuestion(e,'q3')} question="Czy można zarezerwować miejsce w Odskoczni na konkretną godzinę?"/>
-            <FaqComponentA answer="kaka3" />
+            <FaqComponentA answer="Jak najbardziej. Dokonanie rezerwacji możliwe jest przez system rezerwacyjny na naszej stronie internetowej.Znajdziecie go tutaj - DODAJ TUTAJ LINK FILIP! " />
             <FaqComponentQ onClick={(e)=>showQuestion(e,'q4')} question="Jak długo macie otwarte?"/>
-            <FaqComponentA answer="kaka4" />
+            <FaqComponentA answer="Jesteśmy otwarci od poniedziałku do piątku w godzinach 15:00 - 20:00 a w weekend od 10:00 do 20:00" />
+            <FaqComponentQ onClick={(e)=>showQuestion(e,'q5')} question="Czy można u was kupić skarpetki antypoślizgowe?"/>
+            <FaqComponentA answer="Oczywiście. Skarpetki są do nabycia na miejscu w cenie 10 zł." />
         </div>
     </div>
   )
