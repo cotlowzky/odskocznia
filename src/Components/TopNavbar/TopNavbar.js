3958db7e3c6155481f1f94040cc9dee380a9d7ab
cotlowzky <cotlowzky@gmail.com>
2022-08-21 00:00:52 +0200

    add link to pdf with rules

--- src/Components/TopNavbar/TopNavbar.js
+++ src/Components/TopNavbar/TopNavbar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './TopNavbar.css'
 import { Link } from "react-router-dom"
+import regulamin from '../../assets/pdf/regulamin_odskocznia.pdf'
+
 export default function TopNavbar() {
   return (
     <header className='flex-row flex-justify-around h-5 background-navbar flex-items-center w-70 shadow p-2'>
         <Link to="/" className='font-15 text-upper font-montserrat-medium'>logo</Link>
         <a href="#" className='font-15 text-upper font-montserrat-medium'>zasady bezpieczeństwa</a>
-        <a href="#" className='font-15 text-upper font-montserrat-medium'>regulamin</a>
+        <a href={regulamin} target="_blank" rel="noreferrer" className='font-15 text-upper font-montserrat-medium'>regulamin</a>
         <Link to="/faq" className='font-15 text-upper font-montserrat-medium'>faq</Link>
         <a href="#" className='font-15 text-upper font-montserrat-medium'>regulamin urodzin</a>
         <Link to="/hop-mania" className='font-15 text-upper font-montserrat-medium'>hopmania</Link>
