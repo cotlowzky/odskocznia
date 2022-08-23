11bd7a287d38a97e95ff1997986dca5bbccd6ca0
cotlowzky <cotlowzky@gmail.com>
2022-08-23 17:16:42 +0200

    add logo to navbar

--- src/Components/TopNavbar/TopNavbar.js
+++ src/Components/TopNavbar/TopNavbar.js
@@ -3,11 +3,13 @@ import './TopNavbar.css'
 import { Link } from "react-router-dom"
 import regulamin from '../../assets/pdf/regulamin_odskocznia.pdf'
 import regulaminUrodzinowy from '../../assets/pdf/regulamin-urodzinowy.pdf'
-
+import logo from '../../assets/img/logo.png'
 export default function TopNavbar() {
   return (
     <header className='flex-row flex-justify-around h-5 background-navbar flex-items-center w-70 shadow p-2'>
-        <Link to="/" className='font-15 text-upper font-montserrat-medium'>logo</Link>
+        <Link to="/" className='font-15 text-upper font-montserrat-medium'>
+          <img src={logo} height="50px"/>
+        </Link>
         <Link to="/zasady" className='font-15 text-upper font-montserrat-medium'>zasady bezpieczeństwa</Link>
         <a href={regulamin} target="_blank" rel="noreferrer" className='font-15 text-upper font-montserrat-medium'>regulamin</a>
         <Link to="/faq" className='font-15 text-upper font-montserrat-medium'>faq</Link>
